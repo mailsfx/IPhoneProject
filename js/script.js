d6377ba04ac536ebdcea231bb9d5f7877390bf5c
mailsfx <michak2204@gmail.com>
2025-10-18 19:25:19 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// ============ anchor-links ==============
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (anchor of anchors) {
+  if (anchor) {
+    anchor.addEventListener('click', function(e){
+      e.preventDefault();
+      anchorId = this.getAttribute('href');
+      if (anchorId != "#")
+      document.querySelector(anchorId).scrollIntoView({
+        behavior: 'smooth', block: 'start'
+      });
+    });
+  }
+}
+
+
+
